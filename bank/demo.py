516c536e86675ea9687efd8c7540da0ea82ae5c5
sbrener1610 <sbrener1610@gmail.com>
2023-07-12 17:48:28 +0200

    Comments and eliminating error sources

--- bank/demo.py
+++ bank/demo.py
@@ -1,96 +1,117 @@
 import json
 import datetime
 
-def load_clients():
 
-    with open('clients.json') as file:
-        data=json.load(file)
-        return(data)
-    
-def save_clients(clients):
-    with open("clients.json","w") as file:
+def load_clients():  #loads _clients_ from database
+    with open("clients.json") as file:
+        data = json.load(file)
+        return data
+
+
+def save_clients(clients):   #updates database
+    with open("clients.json", "w") as file:
         json.dump(clients, file, indent=4)
 
-def get_age(dob):
-    date_of_birth = datetime.datetime.strptime(dob,"%Y-%m-%d")
-    now=datetime.datetime.now()
-    birthday_this_year=datetime.datetime(year=now.year,month=date_of_birth.month,day=date_of_birth.day)
-    if now>birthday_this_year:
-        age=now.year-date_of_birth.year
+
+def get_age(dob):  #full years from _dob_ to today
+    date_of_birth = datetime.datetime.strptime(dob, "%Y-%m-%d")
+    now = datetime.datetime.now()
+    birthday_this_year = datetime.datetime(
+        year=now.year, month=date_of_birth.month, day=date_of_birth.day
+    )
+    if now > birthday_this_year:
+        age = now.year - date_of_birth.year
     else:
-        age=now.year-date_of_birth.year-1
+        age = now.year - date_of_birth.year - 1
     return age
 
-def add_client(clients,name,dob,balance):
-    if clients:
-        client_id=clients[-1]["client_id"]+1
+
+def add_client(clients, name, dob, balance):
+    if clients:    #if clients not empty, takes the maximum available ID and adds 1
+        client_id = clients[-1]["client_id"] + 1
     else:
-        client_id=1
-    clients.append({
-        "client_id":client_id,
-        "name":name,
-        "dob":dob,
-        "balance":balance
-    })
-    save_clients(clients)
+        client_id = 1
+    clients.append(         #Adds a client
+        {"client_id": client_id, "name": name, "dob": dob, "balance": balance}
+    )
+    save_clients(clients)  #update database
     print("Client added successfully.")
 
-def update_client(clients,client_id, name=None,dob=None, balance=None):
-    for client in clients:
-        if client["client_id"]==client_id:
-            if name:
-                client.update({"name":name})
-            if dob:
-                client.update({"dob":dob})
-            if balance:
-                client.update({"balance":balance})
-                #client["balance"]=balance
-            save_clients(clients)
-            print("Client updated successfully.")
-            return
-    print("Customer does not exist.")
+def find_client(clients,client_id):
+    list_of_clients=list(map(lambda x:x["client_id"], clients))  #list of id's
+    try:
+       return list_of_clients.index(client_id)   #returns the index in the database of the user with given ID. None if not found
+    except ValueError:
+        return None
+
+def update_client(clients, client_id, name=None, dob=None, balance=None):
+    #for client in clients:
+    #    if client["client_id"] == client_id:
+    j=find_client(clients,client_id)
+    if j==None:   #checks id client exists, 
+        print("Customer does not exist.")
+    else:    
+        if name:
+            clients[j].update({"name": name})
+        if dob:
+            clients[j].update({"dob": dob})
+        if balance:
+            clients[j].update({"balance": balance})
+                # client["balance"]=balance
+        save_clients(clients)
+        print("Client updated successfully.")
+        
     
+
 
 def delete_client(clients, client_id):
+    j=find_client(clients,client_id)
+    if j==None:   #checks id client exists, 
+        print("Customer does not exist.")
+    else:
+        clients.pop(j)
+        save_clients(clients)
+        print("Client deleted successfully.")
+        
 
-    for client in clients:
-        if client["client_id"]==client_id:
-            clients.remove(client)
-            save_clients(clients)
-            print("Client deleted successfully.")
-            return
-    print("Customer does not exist.")
-    
 
-def display_client(clients,client_id):
-    for client in clients:
-        if client["client_id"]==client_id:
-            print(f"Client ID: {client['client_id']}")
-            print(f"Client Name: {client['name']}")
-            print(f"Client DOB: {client['dob']}")
-            print(f"Client Age: {get_age(client['dob'])}")
-            print(f"Client Balance: {client['balance']}")
-            return
-    print("Customer does not exist.")
+def display_client(clients, client_id):
+    j=find_client(clients,client_id)
+    if j==None:   #checks id client exists, 
+        print("Customer does not exist.")
+    else:
+            #print(f"Client ID: {client['client_id']}")
+        print(f"Client Name: {clients[j]['name']}")
+        print(f"Client DOB: {clients[j]['dob']}")
+        print(f"Client Age: {get_age(clients[j]['dob'])}")
+        print(f"Client Balance: {clients[j]['balance']}")
+       
+
 
 def display_total(clients):
-    total=sum(client['balance'] for client in clients)
+    total = sum(client["balance"] for client in clients)
     print("Total bank balance:", total)
 
-def make_trasfer(clients, id_from, id_to, transfer_sum):
-    for client_from in clients:
-        if client_from["client_id"]==id_from:
-            for client_to in clients:
-                if client_to["client_id"]==id_to:
-                    client_to["balance"]+=transfer_sum
-                    client_from["balance"]-=transfer_sum
-                    save_clients(clients)
-                    print(f"Successfully transered {transfer_sum} from client with ID {id_from} to client with id {id_to}")
-                    return
-            print("Receipient does not exist.")    
-            return
-    print("Sender does not exist.")  
 
+def make_trasfer(clients, id_from, id_to, transfer_sum,limit=0): #limit is never actually different from 0. At least now.
+    j_from=find_client(clients,id_from)  #checks id client exists
+    j_to=find_client(clients,id_to)
+    if j_from==None:   
+        print("Sender does not exist.")
+       
+    elif j_to==None:   #checks id client exists,
+        print("Receipient does not exist.")
+    else:
+        if clients[j_from]["balance"]-transfer_sum>limit:  #checks limit
+            clients[j_to]["balance"] += transfer_sum
+            clients[j_from]["balance"] -= transfer_sum
+            save_clients(clients)
+            print(
+                f"Successfully transered {transfer_sum} from client with ID {id_from} to client with ID {id_to}"
+            )
+        else:
+            print("Not enough funds")
+    
 
 
 """clients=load_clients()
--- bank/demo2.py
+++ bank/demo2.py
@@ -2,108 +2,174 @@ import demo as banking
 import datetime
 import getch
 
+
 def main():
     while True:
-
-        print("""1: Add
+        print(
+            """1: Add
 2: Update
 3: Delete
 4: Display
 5: Total
 6: Transfer
-7: Exit""")
+7: Exit"""
+        )
 
         print("\nSelect an option: ")
         option = getch.getch()
 
         if option == "1":
             name = input("Name: ")
-            while True:
+            while True:  #Checks DOB is valid, repeats prompt until it is
                 dob = input("DOB: ")
                 try:
-                    datetime.datetime.strptime(dob,"%Y-%m-%d")
+                    datetime.datetime.strptime(dob, "%Y-%m-%d")
                     break
                 except ValueError:
                     print("Try again")
-                
+
             while True:
-                try:
+                try:  #Checks balance is valid, repeats prompt until it is
                     balance = float(input("Balance: "))
                     break
                 except ValueError:
                     print("Try again")
             banking.add_client(clients, name, dob, balance)
-            #print("Client added successfully.")
+            # print("Client added successfully.")
         elif option == "2":
-            client_id=int(input("Client ID: "))
+            while True:    #Checks ID is valid, repeats prompt until it is
+                try:
+                    client_id = int(input("Client ID: "))
+                    break
+                except ValueError:
+                    print("Try again")
+            if banking.find_client(clients,client_id)==None:   #Checks user exists. If not - back to menu
+                print("Customer does not exist.")    
+                continue        
             name = input("Name: ")
-            while True:
+            while True:   #again checks dob and balance for validity
                 dob = input("DOB: ")
                 if not dob:
                     break
                 try:
-                    datetime.datetime.strptime(dob,"%Y-%m-%d")
+                    datetime.datetime.strptime(dob, "%Y-%m-%d")
                     break
                 except ValueError:
                     print("Try again")
             while True:
-                    balance = input("Balance: ")
-                    if not balance:
-                        break
-                    try:    
-                       balance=float(balance)
-                       break
-                    except TypeError:
-                        print("Try again")
-            banking.update_client(clients,client_id,name,dob,balance)
-            #print("Client updated successfully.")
+                balance = input("Balance: ")
+                if not balance:
+                    break
+                try:
+                    balance = float(balance)
+                    break
+                except ValueError:
+                    print("Try again")
+            banking.update_client(clients, client_id, name, dob, balance)
+            # print("Client updated successfully.")
         elif option == "3":
-            while True:
+            while True:  #checks id is an integer. Check for existence is in the delete_client function
                 try:
-                    client_id=int(input("Client ID: "))
+                    client_id = int(input("Client ID: "))
                     break
                 except ValueError:
                     print("Try again")
-            banking.delete_client(clients,client_id)
-            #print("Client deleted successfully.")
+            banking.delete_client(clients, client_id)
+            # print("Client deleted successfully.")
         elif option == "4":
-            while True:
+            while True:  #same as before
                 try:
-                    client_id=int(input("Client ID: "))
+                    client_id = int(input("Client ID: "))
                     break
                 except ValueError:
                     print("Try again")
-            banking.display_client(clients,client_id)
-            
+            banking.display_client(clients, client_id)
+
         elif option == "5":
             banking.display_total(clients)
-        elif option=="6":
-            while True:
+        elif option == "6":
+            while True:  #checks both id's to be integers and checks if both users exist. If not integer prompts again, if does not exist - to the main menu
                 try:
-                    id_from=int(input("Sender ID: "))
+                    id_from = int(input("Sender ID: "))
                     break
                 except ValueError:
                     print("Try again")
+            if banking.find_client(clients,id_from)==None:
+                print("Customer does not exist.")    
+                continue 
             while True:
                 try:
-                    id_to=int(input("Receipient ID: "))
-                    if id_to==id_from:
+                    id_to = int(input("Receipient ID: "))
+                    if id_to == id_from:
                         print("Don't waste my time.")
                         continue
                     break
                 except ValueError:
                     print("Try again")
-            while True:
+            if banking.find_client(clients,id_to)==None:
+                print("Customer does not exist.")    
+                continue 
+            while True:  #checks transfer_sum is float. Prompts again if not.
                 try:
-                    transfer_sum=float(input("Sum: "))
+                    transfer_sum = float(input("Sum: "))
                     break
                 except ValueError:
                     print("Try again")
-            
-            banking.make_trasfer(clients,id_from,id_to,transfer_sum)
+
+            banking.make_trasfer(clients, id_from, id_to, transfer_sum)
         elif option == "7":
             break
 
-clients=banking.load_clients()
+
+clients = banking.load_clients()
+for client in clients:   
+    """should never be necessary unless someone messes with the database manually.
+       checking for validity of all database entries and demands to change them.
+    """
+    if not isinstance(client["client_id"],int):
+        while True:
+            try:
+                client.update({"client_id":int(input(f"ID {client['client_id']} is not an integer. Change: "))})
+                break
+            except ValueError:
+                print("Try again")    
+    if not isinstance(client["balance"],float):
+        while True:
+            try:
+                client.update({"balance":float(input(f"Balance of client ID {client['client_id']}, {client['balance']} is not a float. Change: "))})
+                break
+            except ValueError:
+                print("Try again")    
+    try:
+        datetime.datetime.strptime(client["dob"], "%Y-%m-%d")
+    except ValueError:
+        while True:
+            try:
+                client.update({"dob":datetime.datetime.strftime(datetime.datetime.strptime(input(f"DOB of client ID {client['client_id']}, {client['dob']} is not a valid date. Change: "), "%Y-%m-%d"),"%Y-%m-%d")})
+                break
+            except ValueError:
+                print("Try again")     
+banking.save_clients(clients)   
+clients.sort(key=lambda x:x["client_id"])  #in case ID's are not in ascending order after some un forseen actions
+MAX_id=clients[-1]["client_id"]  #maximal ID
+list_of_id=list(map(lambda x:x["client_id"], clients))  #creates list of ID's
+if len(set(list_of_id))!=len(clients):   #checks for double ID's
+    print("Fatal error, matching id's. Fix manually (y)/by default (n)")
+    
+    while True:
+        option=getch.getch()
+        if option.lower()=="y":       
+           exit()
+        if option.lower()=="n":  #scans list of ID's for repeated entries and changes the first one to the maximal ID+1
+            for i in list_of_id:
+                if list_of_id.count(i)!=1:
+                    j=list_of_id.index(i)
+                    clients[j].update({"client_id":MAX_id+1})
+                    MAX_id+=1
+                    list_of_id.pop(j)
+                    list_of_id.insert(j,MAX_id)
+            print("Fixed!")
+            break
+clients.sort(key=lambda x:x["client_id"]) #final sort
+banking.save_clients(clients) #and save database
 main()
-        
